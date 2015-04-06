e4920186eed495d46c947c92df098a4c50dacaff
irkanu <irkanu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-06 23:10:53 +0000

    release 1.1.1 fixed screenshot bug
    git-svn-id: https://plugins.svn.wordpress.org/simple-facebook-twitter-widget/tags/1.1.1@1129101 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,78 @@
+=== Simple Facebook Page Widget & Shortcode ===
+Contributors: irkanu
+Donate link: https://donorbox.org/dylan-ryan-simple-facebook-page-widget
+Tags: facebook, widget, shortcode, facebook page, facebook widget, facebook page widget, facebook page shortcode, social media
+Requires at least: 3.0.0
+Tested up to: 4.1.1
+Stable tag: 1.1.1
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl.html
+
+Easily display Facebook's new Page feed using a widget or shortcode.
+
+== Description ==
+= Overview =
+This plugin uses Facebook Graph API v2.3 to generate a Facebook Page feed. Many sites are currently running Facebook Like Boxes which will become **deprecated on June 23rd, 2015**. The Page Plugin lets you easily embed and promote any Facebook Page on your website. Just like on Facebook, your visitors can like and share the Page without having to leave your site.
+
+> <strong>Widget Instructions</strong><br/>
+> Add a customizable Facebook widget through `Appearance -> Widgets` and choose “Simple Facebook Page Widget.”
+> Update the “Facebook Page URL” and click `Save`.
+>
+> See the <a href=“https://wordpress.org/plugins/simple-facebook-twitter-widget/screenshots/”>screenshots</a> for examples.
+
+> <strong>Shortcode Instructions</strong><br/>
+> Base Shortcode Example:
+> `[facebook-page href="facebook"]`
+> 
+> Custom Shortcode Example:
+> `[facebook-page href="facebook" width="300" height="800" hide_cover="false" show_facepile="false" show_posts="true"]`
+> 
+> **Note:** If your *Facebook Page URL* is https://facebook.com/facebook then please enter `facebook` as the href argument.
+
+= Support =
+
+Support is offered on the <a href=“https://wordpress.org/support/plugin/simple-facebook-twitter-widget”>WordPress Support Forum</a> for free. If you are comfortable with GitHub, then feel free to submit an <a href=“https://github.com/irkanu/simple-facebook-page-widget/issues”>issue</a>. I’ll do my best to answer all support threads as well as issues. Think I’ve done a good job? Consider <a href=“https://donorbox.org/dylan-ryan-simple-facebook-page-widget”>donating</a> $1.
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+1. Download `simple-facebook-page-widget.zip`.
+2. Upload `simple-facebook-page-widget` plugin to the `/wp-content/plugins/` directory through the WordPress plugin interface.
+3. Activate the plugin through the `Plugins` menu in WordPress.
+4. `Optional:` Add a Simple Facebook Page Widget from the `Appearance -> Widgets` menu in WordPress.
+5. `Optional:` Add a [facebook-page href="facebook"] shortcode inside of a page or post.
+
+== Frequently Asked Questions ==
+
+= Do I have to create my own custom Facebook App? =
+
+Currently, no. In the future, perhaps.
+
+== Screenshots ==
+
+1. Frontend view of the Facebook Page widget.
+2. Backend view of the Facebook Page widget.
+3. Frontend view of the Facebook Page shortcode.
+4. Backend view of the Facebook Page shortcode.
+
+== Changelog ==
+
+= 1.1.1 =
+* Fixed screenshot bug
+
+= 1.1.0 =
+* i18n Support
+
+= 1.0.0 =
+* Initial Release
+* Widget & Shortcode Fully Functional
+
+== Donations ==
+
+This plugin was developed to be used free of charge; however, if you do wish to donate, then you can visit the [donate page](https://donorbox.org/dylan-ryan-simple-facebook-page-widget).
+
+== Upgrade Notice ==
+
+= 1.1.0 =
+* Added support for Spanish / Añadido soporte para español
---
+++ simple-facebook-page-plugin.php
@@ -0,0 +1,219 @@
+<?php
+/*
+Plugin Name:    Simple Facebook Page Widget
+Plugin URI:     https://wordpress.org/plugins/simple-facebook-page-widget/
+Description:    Shows the Facebook Page feed in a sidebar widget and/or via shortcode.
+Version:        1.1.1
+Author:         Dylan Ryan
+Author URI:     https://profiles.wordpress.org/irkanu
+Text Domain:    simple-facebook-twitter-widget
+GitHub URI:     https://github.com/irkanu/simple-facebook-page-widget
+GitHub Branch:  master
+License:        GPL v3
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * Deny Direct Access
+ */
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Plugin Constants
+ */
+if ( ! defined( 'SIMPLE_FACEBOOK_PAGE_WIDGET_VERSION' ) ) {
+	define( 'SIMPLE_FACEBOOK_PAGE_WIDGET_VERSION', '1.1.1' );
+}
+if ( ! defined( 'SIMPLE_FACEBOOK_PAGE_WIDGET_DIRECTORY' ) ) {
+    define( 'SIMPLE_FACEBOOK_PAGE_WIDGET_DIRECTORY', plugin_dir_url( __FILE__ ) );
+}
+if ( ! defined( 'SIMPLE_FACEBOOK_PAGE_I18N' ) ) {
+    define( 'SIMPLE_FACEBOOK_PAGE_I18N', 'simple-facebook-twitter-widget' );
+}
+
+/**
+ * Enqueue Facebook script required for the plugin.
+ *
+ * @since 1.0.0
+ */
+add_action( 'wp_enqueue_scripts', 'sfpp_enqueue_scripts' );
+function sfpp_enqueue_scripts() {
+	wp_enqueue_script( 'sfpp-fb-root', SIMPLE_FACEBOOK_PAGE_WIDGET_DIRECTORY . 'js/simple-facebook-page-root.js' , array( 'jquery' ) );
+}
+
+/**
+ * Create the [facebook-page] shortcode.
+ *
+ * @since 1.0.0
+ * @param $atts array href, width, height, hide_cover, show_facepile, show_posts
+ * @return string Outputs the Facebook Page feed via shortcode.
+ */
+add_shortcode( 'facebook-page', 'sfpp_shortcode' );
+function sfpp_shortcode( $atts ) {
+
+	$output = '';
+
+	$facebook_page_atts = shortcode_atts( array(
+		'href'          => '',
+		'width'         => '340',
+		'height'        => '500',
+		'hide_cover'    => 'false',
+		'show_facepile' => 'false',
+		'show_posts'    => 'true'
+	), $atts );
+
+	$output .= '<div class="fb-page" ';
+	$output .= 'data-href="https://facebook.com/' . wp_kses_post( $facebook_page_atts['href'] ) . '" ';
+	$output .= 'data-width="' . wp_kses_post( $facebook_page_atts['width'] ) . '" ';
+	$output .= 'data-height="' . wp_kses_post( $facebook_page_atts['height'] ) . '" ';
+	$output .= 'data-hide-cover="' . wp_kses_post( $facebook_page_atts['hide_cover'] ) . '" ';
+	$output .= 'data-show-facepile="' . wp_kses_post( $facebook_page_atts['show_facepile'] ) . '" ';
+	$output .= 'data-show-posts="' . wp_kses_post( $facebook_page_atts['show_posts'] ) . '">';
+	$output .= '</div>';
+
+	return $output;
+
+}
+
+/**
+ * Registers the SFPP_Widget widget class.
+ *
+ * @since 1.0.0
+ */
+require_once( 'includes/class-simple-facebook-page-plugin-widget.php' );
+add_action( 'widgets_init', function () {
+	register_widget( 'SFPP_Widget' );
+} );
+
+
+/**
+ * Load the translation PO files.
+ * http://codex.wordpress.org/I18n_for_WordPress_Developers
+ *
+ * @since 1.1.0
+ */
+load_plugin_textdomain( SIMPLE_FACEBOOK_PAGE_I18N, false, SIMPLE_FACEBOOK_PAGE_WIDGET_DIRECTORY . 'languages' );
+
+
+/**
+ * Register the Settings Page & Settings
+ *
+ * TODO: Add this back after custom App ID's are ready.
+ */
+/*
+function sfpp_add_plugin_options_page(){
+	add_options_page('Simple Facebook Page Settings', 'Simple Facebook Page Settings', 'manage_options', 'simple-facebook-page-settings', 'sfpp_create_admin_page');
+}
+function sfpp_register_options() {
+	register_setting('sfpp_options', 'sfpp_settings', 'sfpp_validation');
+}
+if( is_admin() ) {
+	add_action('admin_menu', 'sfpp_add_plugin_options_page');
+	add_action('admin_init', 'sfpp_register_options');
+}
+*/
+
+/**
+ * Validate Settings input
+ *
+ * TODO: Add this back after custom App ID's are ready.
+ *
+ * @param $input
+ *
+ * @return
+ */
+/*
+function sfpp_validation( $input ){
+	if ( ! empty( $input ) ) {
+		$input['sfpp_app_id'] = wp_kses_post( $input['sfpp_app_id'] );
+	}
+	return $input;
+}
+*/
+
+/**
+ * Register activation hook to populate settings
+ *
+ * TODO: Add this back after custom App ID's are ready.
+ */
+/*
+register_activation_hook(__FILE__, 'sfpp_activation');
+function sfpp_activation() {
+	$sfpp_options = get_option('sfpp_settings');
+	update_option('sfpp_settings', $sfpp_options);
+}
+*/
+
+/**
+ * Build the Settings Page
+ *
+ * TODO: Allow users to add their own App ID's.
+ * TODO: Nonce it up.
+ * if ( function_exists('wp_nonce_field') )
+ * wp_nonce_field('facebook-page-settings-update_general_options_' . $sfpp_options);
+ */
+/*
+function sfpp_create_admin_page(){
+
+	function sfpp_check_admin_referer() {
+		check_admin_referer('facebook-page-settings-update_general_options_' . $sfpp_options);
+	}
+
+	ob_start(); ?>
+
+	<div class="wrap">
+		<h2>Simple Facebook Page Settings</h2>
+
+		<form action="options.php" method="post">
+
+			<?php settings_fields('sfpp_options'); ?>
+			<?php $sfpp_options = get_option('sfpp_settings', SIMPLE_FACEBOOK_PAGE_I18N); ?>
+			<?php update_option('sfpp_settings', $sfpp_options); ?>
+
+			<table class="form-table">
+				<tr valign="top"><th scope="row"><label class="description" for="sfpp_settings[sfpp_app_id]"><?php _e('Enter App ID:', SIMPLE_FACEBOOK_PAGE_I18N); ?></label></th></tr>
+				<tr valign="top"><th scope="row"><input type="text" id="sfpp_settings[sfpp_app_id]" name="sfpp_settings[sfpp_app_id]" value="<?php echo $sfpp_options['sfpp_app_id']; ?>"/></th></tr>
+				<tr valign="top"><th scope="row"><input type="submit" class="button-primary" value="<?php _e('Save Settings', SIMPLE_FACEBOOK_PAGE_I18N); ?>"/></th></tr>
+
+				<?php
+
+				?>
+			</table>
+	</form>
+
+	</div>
+	<?php
+	echo ob_get_clean();
+}
+*/
+/**
+ * Quick navigation link to settings page from plugin list.
+ *
+ * TODO: Add this back after custom App ID's are ready.
+ */
+/*
+add_filter( 'plugin_action_links', 'sfpp_settings_link', 2, 2 );
+function sfpp_settings_link( $actions, $file ) {
+
+	if ( false !== strpos( $file, 'simple-facebook-page-plugin' ) ) {
+		$actions['settings'] = '<a href="options-general.php?page=simple-facebook-page-settings">Settings</a>';
+	}
+
+	return $actions;
+
+}
+*/
